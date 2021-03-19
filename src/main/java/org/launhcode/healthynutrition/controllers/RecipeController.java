2f6b1933ab7f1d6d41044c8c149446394a6bcea5
Prashanthi Chirunomula <chirunomulap@gmail.com>
2021-03-19 17:16:33 -0500

    Saving recipe functionality implemented

--- src/main/java/org/launhcode/healthynutrition/controllers/RecipeController.java
+++ src/main/java/org/launhcode/healthynutrition/controllers/RecipeController.java
@@ -3,11 +3,16 @@ package org.launhcode.healthynutrition.controllers;
 import org.launhcode.healthynutrition.data.RecipeCategoryRepository;
 import org.launhcode.healthynutrition.data.RecipeRepository;
 import org.launhcode.healthynutrition.models.Recipe;
+import org.launhcode.healthynutrition.models.RecipeCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("recipe")
@@ -23,5 +28,21 @@ public class RecipeController {
         model.addAttribute("categories", recipeCategoryRepository.findAll());
         model.addAttribute(new Recipe());
         return "recipe/create";
+    }
+    @PostMapping("create")
+    public String processCreateRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+                                    Errors errors, Model model,
+                                    @RequestParam(required = false) List<Integer> categories) {
+        if (errors.hasErrors() || categories.isEmpty() ) {
+            model.addAttribute("title", "Create Recipe");
+            model.addAttribute("categories", recipeCategoryRepository.findAll());
+            model.addAttribute(new Recipe());
+            return "recipe/create";
+        }
+
+        List<RecipeCategory> recipeCategories =(List<RecipeCategory>) recipeCategoryRepository.findAllById(categories);
+        newRecipe.setCategories(recipeCategories);
+        recipeRepository.save(newRecipe);
+        return "redirect:/";
     }
 }
