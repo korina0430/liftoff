073576dd6cd5e02123dba24ab9fe234f3137620c
prashanthich <72408815+prashanthich@users.noreply.github.com>
2021-03-21 17:00:55 -0500

    Create recipe v1 (#21)
    * added dependencies for sql and jpa
    
    * Entity models added for create recipe
    
    * controller and form design for recipe creation implemented
    
    * Saving recipe functionality implemented

---
+++ src/main/java/org/launhcode/healthynutrition/controllers/RecipeController.java
@@ -0,0 +1,48 @@
+package org.launhcode.healthynutrition.controllers;
+
+import org.launhcode.healthynutrition.data.RecipeCategoryRepository;
+import org.launhcode.healthynutrition.data.RecipeRepository;
+import org.launhcode.healthynutrition.models.Recipe;
+import org.launhcode.healthynutrition.models.RecipeCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("recipe")
+public class RecipeController {
+    @Autowired
+    private RecipeRepository recipeRepository;
+    @Autowired
+    private RecipeCategoryRepository recipeCategoryRepository;
+
+    @GetMapping("create")
+    public String displayCreateRecipeForm(Model model){
+        model.addAttribute("title", "Create Recipe");
+        model.addAttribute("categories", recipeCategoryRepository.findAll());
+        model.addAttribute(new Recipe());
+        return "recipe/create";
+    }
+    @PostMapping("create")
+    public String processCreateRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+                                    Errors errors, Model model,
+                                    @RequestParam(required = false) List<Integer> categories) {
+        if (errors.hasErrors() || categories.isEmpty() ) {
+            model.addAttribute("title", "Create Recipe");
+            model.addAttribute("categories", recipeCategoryRepository.findAll());
+            model.addAttribute(new Recipe());
+            return "recipe/create";
+        }
+
+        List<RecipeCategory> recipeCategories =(List<RecipeCategory>) recipeCategoryRepository.findAllById(categories);
+        newRecipe.setCategories(recipeCategories);
+        recipeRepository.save(newRecipe);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/org/launhcode/healthynutrition/models/Recipe.java
@@ -0,0 +1,97 @@
+package org.launhcode.healthynutrition.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue
+    private int id;
+    @NotBlank(message = "Name/Title is required")
+    @Size(min = 3, max = 50, message = "Name must be in 3 to 50 characters")
+    private String name;
+    @NotBlank(message = "Description is required")
+    @Size(min = 5, max = 150, message = "Description must be in 5 to 150 characters")
+    private String description;
+    @Min(value=1, message = "Minimum calories should be 1")
+    private int calories;
+    @NotBlank(message = "Preparation steps are required")
+    @Size(min = 10, max = 5000, message = "Directions must be in 10 to 5000 characters")
+    private String directions;
+    @NotBlank(message = "Serving units is required")
+    @Size(min = 5, max = 100, message = "Serving units must be in 5 to 100 characters")
+    private String servingUnits;
+    @ManyToMany
+    private List<RecipeCategory> categories;
+
+    public Recipe() {}
+
+    public Recipe(String name, String description, int calories, String directions,
+                  String servingUnits, List<RecipeCategory> categories) {
+        this.name = name;
+        this.description = description;
+        this.calories = calories;
+        this.directions = directions;
+        this.servingUnits = servingUnits;
+        this.categories = categories;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public String getServingUnits() {
+        return servingUnits;
+    }
+
+    public void setServingUnits(String servingUnits) {
+        this.servingUnits = servingUnits;
+    }
+
+    public List<RecipeCategory> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<RecipeCategory> categories) {
+        this.categories = categories;
+    }
+}
