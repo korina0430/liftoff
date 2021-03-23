b7f5ea11f4227c7e77005bfa00a03be343a656a4
Rexzellius <katlynmmills@gmail.com>
2021-03-22 20:32:27 -0500

    Removed bin files

--- src/main/java/org/launhcode/healthynutrition/controllers/FoodController.java
+++
@@ -1,9 +0,0 @@
-//package org.launhcode.healthynutrition.controllers;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/foods")
-//public class FoodController {
-//
-//}
--- src/main/java/org/launhcode/healthynutrition/controllers/UserDayController.java
+++
@@ -1,8 +0,0 @@
-//package org.launhcode.healthynutrition.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
-//public class UserDayController {
-//
-//}
