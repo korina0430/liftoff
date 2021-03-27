a1db5bd0d751ed7bc7c1b17ba1c0a5b6d39efd32
prashanthich <72408815+prashanthich@users.noreply.github.com>
2021-03-27 16:22:33 -0500

    Edit recipe functionality completed

--- src/main/java/org/launhcode/healthynutrition/controllers/RecipeController.java
+++ src/main/java/org/launhcode/healthynutrition/controllers/RecipeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("recipe")
@@ -26,25 +27,44 @@ public class RecipeController {
     @GetMapping("create")
     public String displayCreateRecipeForm(Model model){
         model.addAttribute("title", "Create Recipe");
+        model.addAttribute("categoryError", "");
         model.addAttribute("categories", recipeCategoryRepository.findAll());
         model.addAttribute(new Recipe());
         return "recipe/create";
     }
 
     @PostMapping("create")
-    public String processCreateRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+    public String processCreateOrEditRecipeForm(@ModelAttribute @Valid Recipe recipe,
                                     Errors errors, Model model,
                                     @RequestParam(required = false) List<Integer> categories) {
-        if (errors.hasErrors() || categories.isEmpty() ) {
+
+        if (errors.hasErrors() || categories == null) {
             model.addAttribute("title", "Create Recipe");
+            if (recipe.getId() > 0){
+                model.addAttribute("title", "Edit Recipe");
+            }
+            if (categories == null){
+                model.addAttribute("categoryError", "Category is required");
+
+            }
+
             model.addAttribute("categories", recipeCategoryRepository.findAll());
-            model.addAttribute(newRecipe);
+            model.addAttribute(recipe);
             return "recipe/create";
         }
 
         List<RecipeCategory> recipeCategories =(List<RecipeCategory>) recipeCategoryRepository.findAllById(categories);
-        newRecipe.setCategories(recipeCategories);
-        recipeRepository.save(newRecipe);
+        recipe.setCategories(recipeCategories);
+        recipeRepository.save(recipe);
         return "redirect:/";
     }
+    @GetMapping("edit")
+    public String getRecipeForm(Model model, @RequestParam int id){
+        model.addAttribute("title", "Edit Recipe");
+        model.addAttribute("categories", recipeCategoryRepository.findAll());
+        Optional<Recipe> existingRecipe =   recipeRepository.findById(id);
+        model.addAttribute(existingRecipe.get());
+    return "recipe/create";
+    }
+
 }
