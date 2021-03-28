e51e8b0fe9559f4540832b4d1fddb8398b7a4f17
Rama <ramagopal80@gmail.com>
2021-03-28 13:46:28 -0500

    Signup Page with Authentication completed

---
+++ src/main/java/org/launhcode/healthynutrition/data/UserRepository.java
@@ -0,0 +1,7 @@
+package org.launhcode.healthynutrition.data;
+import org.launhcode.healthynutrition.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <User, Integer> {
+   User findByEmail(String email);
+}
---
+++ src/main/java/org/launhcode/healthynutrition/models/User.java
@@ -0,0 +1,85 @@
+package org.launhcode.healthynutrition.models;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+
+    @NotNull
+    private String firstName;
+    private String lastName;
+    @NotNull
+    @Email
+    private String email;
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User(){}
+
+    public User(String firstName, String lastName, String email, String password){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String email) {
+        this.email = email;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(Id, user.Id) &&
+                Objects.equals(getFirstName(), user.getFirstName()) &&
+                Objects.equals(getLastName(), user.getLastName()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(pwHash, user.pwHash);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, getFirstName(), getLastName(), getEmail(), pwHash);
+    }
+
+}
