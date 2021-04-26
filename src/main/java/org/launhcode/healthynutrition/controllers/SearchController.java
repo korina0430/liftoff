826e4be3c768b7637000725cf8683b2bef8572c1
Dhwani Patel <72528017+Dhwani0712@users.noreply.github.com>
2021-04-26 18:34:55 -0500

    Added Search functionality and Create Post/ All Recipes links work. (#37)
    * Added Search functionality and Create Post/ All Recipes links work.
    
    * Reviewed Comments.

---
+++ src/main/java/org/launhcode/healthynutrition/controllers/SearchController.java
@@ -0,0 +1,58 @@
+package org.launhcode.healthynutrition.controllers;
+
+import org.launhcode.healthynutrition.data.RecipeRepository;
+import org.launhcode.healthynutrition.models.Recipe;
+import org.launhcode.healthynutrition.models.RecipeData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+   public static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public SearchController() {
+
+        columnChoices.put("all", "All");
+        columnChoices.put("name", "Name");
+        columnChoices.put("description", "Description");
+        columnChoices.put("servingUnits", "ServingUnits");
+
+    }
+
+
+    @GetMapping("")
+    public String search(Model model){
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        Iterable<Recipe> recipes;
+        if (searchTerm.equalsIgnoreCase("all") || searchTerm.equals("")){
+            recipes = recipeRepository.findAll();
+        } else {
+            recipes = RecipeData.findByColumnAndValue(searchType, searchTerm, recipeRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Recipes with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("recipes", recipes);
+
+        return "search";
+    }
+
+}
+
---
+++ src/main/java/org/launhcode/healthynutrition/models/RecipeData.java
@@ -0,0 +1,81 @@
+package org.launhcode.healthynutrition.models;
+
+import java.util.ArrayList;
+
+public class RecipeData {
+
+    public static ArrayList<Recipe> findByColumnAndValue(String column, String value, Iterable<Recipe> allRecipes) {
+
+        ArrayList<Recipe> results = new ArrayList<>();
+
+        if (value.equalsIgnoreCase("all")){
+            return (ArrayList<Recipe>) allRecipes;
+        }
+
+        if (column.equals("all")){
+            results = findByValue(value, allRecipes);
+            return results;
+        }
+        for (Recipe recipe : allRecipes) {
+
+            String searchString = getFieldValue(recipe, column);
+
+            if (searchString != null && searchString.toLowerCase().contains(value.toLowerCase())) {
+                results.add(recipe);
+            }
+        }
+
+
+        return results;
+    }
+
+    public static String getFieldValue(Recipe recipe, String fieldName){
+        String searchValue;
+        switch (fieldName.toLowerCase()) {
+            case "name":
+                searchValue = recipe.getName();
+                break;
+            case "description":
+                searchValue = recipe.getDescription();
+                break;
+            default:
+                searchValue = recipe.getServingUnits();
+        }
+        return searchValue;
+
+    }
+
+    /**
+     * Search all Recipe fields for the given term.
+     *
+     * @param value The search term to look for.
+     * @param allRecipes The list of recipes to search.
+     * @return      List of all recipes with at least one field containing the value.
+     */
+    public static ArrayList<Recipe> findByValue(String value, Iterable<Recipe> allRecipes) {
+        String searchWord = value.toLowerCase();
+
+        ArrayList<Recipe> results = new ArrayList<>();
+
+        for (Recipe recipe : allRecipes) {
+
+            if (recipe.getName().toLowerCase().contains(searchWord)) {
+                results.add(recipe);
+            } else if (recipe.getDescription().toLowerCase().contains(searchWord)) {
+                results.add(recipe);
+            } else if (recipe.getServingUnits().toLowerCase().contains(searchWord)) {
+                results.add(recipe);
+            } else if (recipe.toString().toLowerCase().contains(searchWord)) {
+                results.add(recipe);
+            }
+
+        }
+
+        return results;
+    }
+
+
+}
+
+
+
